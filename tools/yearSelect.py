9bd8f381b020de7a8163f1e9829a071d461ce9b6
Blake Pavel <blakep2012@gmail.com>
2013-04-19 00:06:34 -0500

    added year script to tools

---
+++ tools/yearSelect.py
@@ -0,0 +1,19 @@
+import ast
+import json
+
+f = open('../data/cleanedCrimeData.json', 'rb')
+
+yearlyCrime = []
+YEAR = 12 #Place year here.
+
+for line in f:
+	crime = ast.literal_eval(line)
+	month, day, year = crime['date'].split('/')
+	if int(year) == YEAR:
+		yearlyCrime.append(crime)
+
+f.close()
+
+newFile = open('../data/YEAR_DATA.json', 'wb')
+
+json.dump(yearlyCrime, newFile)
