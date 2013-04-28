ceb802d76e979b1a7911b562bec04166174a9352
Blake Pavel <blakep2012@gmail.com>
2013-04-27 19:03:48 -0500

    added avg ool, and star analyzer

--- functions/analyze.py
+++ functions/analyze.py
@@ -2,7 +2,7 @@ import json
 import math
 import ast
 
-def analyze(business, crimeTypes, crimeData):
+def analyze(business, crimeTypes, crimeData, averages):
 	busLat = business['latitude']
 	busLong = business['longitude']
 	DIFF = .1
@@ -27,14 +27,20 @@ def analyze(business, crimeTypes, crimeData):
 		ratio = float(rating)/float(counters[crimeType])
 		ratios[crimeType] = ratio
 
-	return ratios, counters
+	difference = {}
+	for cat in business['categories']:
+		difference[cat] = business['stars'] - averages[cat]
+
+	return ratios, counters, difference
 
 bus = {"business_id": "rncjoVoEFUJGCUoC1JgnUA", "full_address": "8466 W Peoria Ave\nSte 6\nPeoria, AZ 85345", "open": True, "categories": ["Accountants", "Professional Services", "Tax Services", "Financial Services"], "city": "Peoria", "review_count": 3, "name": "Peoria Income Tax Service", "neighborhoods": [], "longitude": -112.241596, "state": "AZ", "stars": 5.0, "latitude": 33.581867000000003, "type": "business"}
 crimeFile = open('../data/cleanedData/2007LatLng.json', 'rb')
+a = open('../data/ratingAverages.json', 'rb')
+
 crimes = []
 for line in crimeFile:
 	crimes.append(ast.literal_eval(line))
 
 types = ['Burglary']
 
-print analyze(bus, types, crimes)
+print analyze(bus, types, crimes, json.load(a))
